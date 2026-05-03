03d97f122779a7d69707fc21f5a12d661f85ff36
le-saper-vedere <le@saper-vedere.eu>
2026-05-03 22:52:57 +0200

    chore(backend): connect  routers in main and update dependencies

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI                                                                                                          
-                                                                                                                                       
-app = FastAPI()                                                                                                                      
-                  
+from fastapi import FastAPI
+from routers import teams
+from routers import users
+from routers import matches
+from routers import bets
+
+app = FastAPI()
+
+app.include_router(teams.router)
+app.include_router(users.router)
+app.include_router(matches.router)
+app.include_router(bets.router)
+
 
 @app.get("/")
 def root():
